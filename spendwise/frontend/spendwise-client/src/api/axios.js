d5e4c5279cdef31623b975a59587d4edc43c36db
Russell Ferrero <russellferrero00@gmail.com>
2026-01-26 20:35:56 +0800

    fix: add a 401 response interceptor for an endpoint

--- spendwise/frontend/spendwise-client/src/api/axios.js
+++ spendwise/frontend/spendwise-client/src/api/axios.js
@@ -5,4 +5,18 @@ const api = axios.create({
   withCredentials: true,
 });
 
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.config?.url === '/auth/me' && error.response?.status === 401) {
+      return Promise.reject(error);
+    }
+    if (error.response) {
+      console.error('API Error:', error.response.status, error.response.data);
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
